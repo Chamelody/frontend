01ff49e3a85363abb36c27b6717061bc00932661
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2023-10-01 22:03:00 +0900

    feat: add main route

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Main from './ui/main/Main';
 import Home from './ui/home/Home';
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<Home />} />
+                <Route path="/" element={<Main />} />
+                <Route path="/home" element={<Home />} />
             </Routes>
         </BrowserRouter>
     )
