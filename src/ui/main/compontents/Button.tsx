d553e12e8e6424c5d78e88e83a9549dfa58cf533
nub8p <sujin4418618@gmail.com>
2023-11-08 23:02:31 +0900

    button 경로 페이징 구현

--- src/ui/main/compontents/Button.tsx
+++ src/ui/main/compontents/Button.tsx
@@ -1,44 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import FlexContainer from "../../../components/FlexContainer";
 
 type ButtonProps = {
-    content : string;
-    img_file : string;
-    size?: number; 
+  content: string;
+  img_file: string;
+  size?: number;
+  to: string; // 추가된 프로퍼티: 클릭 시 이동할 경로
 };
 
-const Button = ({content, img_file, size = 16} : ButtonProps): JSX.Element => {
-
+const Button = ({ content, img_file, size = 14, to }: ButtonProps): JSX.Element => {
   return (
-    <FlexContainer 
-        justifyContent="space-evenly" 
-        flexDirection="row"
-        flexWrap="nowrap"
-        width="auto"
-        {...{
-            backgroundColor: '#323232',
-            padding: '10px 16px',
-            borderRadius: '100px'
-
-        }}
+    <FlexContainer
+      justifyContent="space-evenly"
+      flexDirection="row"
+      flexWrap="nowrap"
+      width="auto"
+      {...{
+        backgroundColor: '#323232',
+        padding: '10px 16px',
+        borderRadius: '100px',
+        cursor: 'pointer', // 클릭 가능한 커서 스타일 추가
+      }}
     >
-       
-        <p
-            style={{
-                fontSize: {size}+"px",
+      <Link to={to} 
+            style={{ 
+             
+                textDecoration: 'none', 
+                fontSize: `${size}px`,
                 color: "white",
-                margin: '0',
-                fontWeight: 500
-            }}
-            >
-            {content} 
-        </p>     
-        <img 
-            src={img_file} alt='btn'
-            height={(size/1.2) + "px"}
-            width={(size/1.2) + "px"}
+                fontWeight: 500,
+        }}>
+        {content}
+        <img
+          src={img_file} alt='btn'
+          height={`${size}px`} 
+          width={`${size}px`}
         />
-
+      </Link>
     </FlexContainer>
   );
 };
