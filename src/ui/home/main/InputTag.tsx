a4a31182909ff8d49e60820c956640b84f2aae35
nub8p <67677414+nub8p@users.noreply.github.com>
2023-11-17 16:11:46 +0900

    (Home) Improve Description Button Components

--- src/ui/home/main/InputTag.tsx
+++ src/ui/home/main/InputTag.tsx
@@ -1,4 +1,10 @@
 import React from "react";
+import { DivideContainer, DivideItem } from "../../../components/Divider";
+import { useMediaQuery } from "react-responsive";
+import { ResponsiveSizeConst } from "../../../constants/ResponsiveSizeConst";
+import FlexContainer from "../../../components/FlexContainer";
+import WordBlock from "../../main/compontents/WordBlock";
+import Description from "./Description";
 
 type InputTagProps = {
     height: string      // <height>
@@ -8,17 +14,107 @@ const InputTag = ({
     height,
     width
 }: InputTagProps): JSX.Element => {
-    return <div
-        style={{
-            backgroundColor: 'pink',
-            width: width,
-            height: height
-        }}
-    >
-        Input Tag Selection
+    const isMobileScreen = useMediaQuery({
+        maxWidth: ResponsiveSizeConst.MOBILE_SCREEN_MAX_WIDTH
+    });
 
-    </div>
+    const isTabletScreen = useMediaQuery({
+        minWidth: ResponsiveSizeConst.TABLET_SCREEN_MIN_WIDTH,
+        maxWidth: ResponsiveSizeConst.TABLET_SCREEN_MAX_WIDTH
+    });
 
+    let componentLayoutInfo: {
+        Space: number,
+        Title: number,
+        TitleLayout: Array<number>,
+        Description: number,
+    }
+
+    if (isMobileScreen) {
+        componentLayoutInfo = { Space: 2, Title: 3, TitleLayout: [1, 29], Description: 2 };
+    } else if (isTabletScreen) {
+        componentLayoutInfo = { Space: 5, Title: 3, TitleLayout: [1, 15], Description: 2 };
+    } else {
+        componentLayoutInfo = { Space: 1, Title: 2, TitleLayout: [1, 19], Description: 1 };
+    }
+
+
+    const flexContainerStyles = {
+        width: "auto",
+        backgroundColor: "rgba(0, 0, 0, 0.1)",
+        borderRadius: "50px",
+        marginBottom: "8px",
+        marginRight: "5px",
+        paddingRight: "15px",
+    };
+
+    return (
+        <div
+            style={{
+                // backgroundColor: 'pink',
+                width: width,
+                height: height,
+
+            }}
+        >
+            <DivideContainer direction="column" width="100%" height="100%">
+                {/*Space*/}
+                <DivideItem ratio={componentLayoutInfo.Space}></DivideItem>
+
+                {/*타이틀*/}
+                <DivideItem ratio={componentLayoutInfo.Title}>
+                    <DivideContainer direction="row" width="100%" height="100%">
+                        {/*Space*/}
+                        <DivideItem ratio={componentLayoutInfo.TitleLayout[0]}></DivideItem>
+                        <DivideItem ratio={componentLayoutInfo.TitleLayout[1]}>
+                            <div
+                                style={{
+                                    height: "auto",
+                                    width: "auto",
+                                    // paddingLeft: "50px",
+                                    // paddingTop: "50px",
+                                    boxSizing: "border-box",
+                                }}
+                            >
+                                <FlexContainer flexWrap="wrap" alignContent="flex-start">
+                                    <FlexContainer flexWrap="wrap" {...flexContainerStyles}>
+                                        <WordBlock content={"I want to"} mode="default" />
+                                        <WordBlock content={"Chamelody"} mode="lined" />
+                                    </FlexContainer>
+
+                                    <FlexContainer flexWrap="wrap" alignItems="center" {...flexContainerStyles}>
+                                        <WordBlock content={" from"} mode="default" />
+                                        <WordBlock content={" # Current mood"} mode="filled" color="#323232" />
+                                    </FlexContainer>
+
+                                    <FlexContainer flexWrap="wrap" alignItems="center" {...flexContainerStyles}>
+                                        <WordBlock content={" to"} mode="default" />
+                                        <WordBlock content={" # mood I want"} mode="filled" color="#323232" />
+                                    </FlexContainer>
+
+                                </FlexContainer>
+                            </div>
+                        </DivideItem>
+                    </DivideContainer>
+                </DivideItem>
+
+                {/*설명*/}
+                <DivideItem ratio={componentLayoutInfo.Description}>
+                    <div
+                        style={{
+                            width: "100%",
+                            height: "100%",
+
+                            //backgroundColor: "yellow",
+                        }}>
+                        <Description />
+                    </div>
+
+                </DivideItem>
+            </DivideContainer >
+        </div>
+
+    );
 };
 
 export default InputTag;
