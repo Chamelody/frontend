6bf96eaa4e6393d8c24e87e5ba57641d7d2a516c
nub8p <67677414+nub8p@users.noreply.github.com>
2023-11-23 20:32:02 +0900

    Feat: Implement  Header Logo Component

--- src/ui/main/Main.tsx
+++ src/ui/main/Main.tsx
@@ -2,13 +2,14 @@ import React from "react";
 import Scaffold from "../../components/Scaffold";
 import MainInfo from "./information/MainInfo";
 import MainIntro from "./intro/MainIntro";
-
+import HeaderLogo from "./components/HeaderLogo";
 
 const Main = (): JSX.Element => {
-    return <Scaffold 
-    {...{
-        background: 'linear-gradient(180deg, rgba(46,195,100, 1) 85%, rgba(33, 120, 64, 0.9) 100%)'
-    }}>
+    return <Scaffold
+        {...{
+            background: 'linear-gradient(180deg, rgba(46,195,100, 1) 85%, rgba(33, 120, 64, 0.9) 100%)'
+        }}>
+        <HeaderLogo />
         <MainIntro />
         <MainInfo />
     </Scaffold>;
--- src/ui/playlist/Playlist.tsx
+++ src/ui/playlist/Playlist.tsx
@@ -1,10 +1,11 @@
 import React from "react";
 import PlaylistSection from "./PlaylistSection";
-
+import HeaderLogo from "../main/components/HeaderLogo";
 const Playlist = (): JSX.Element => {
-    return (
-        <PlaylistSection></PlaylistSection>
-    );
+    return <>
+        <HeaderLogo />
+        <PlaylistSection />
+    </>
 };
 
 export default Playlist;
